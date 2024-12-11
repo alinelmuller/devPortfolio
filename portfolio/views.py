46c7c7a92e4e1fe92366ba9e3d2da335d9fbd18e
alinelmuller <alinelmuller@gmail.com>
2024-12-10 19:31:26 -0700

    Saving Brach Aline

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,23 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from .forms import RegisterForm
 
 def index(request):
-    return HttpResponse("Welcome to my portfolio!")
+    return render(request, 'index.html')
 
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')  
+    else:
+        form = RegisterForm()
+    return render(request, 'users/register.html', {'form': form})
 
+@login_required
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
